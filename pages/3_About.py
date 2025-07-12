4e08dc3cce76b8fddf09a3a61855f04815d893fc
AnushkaKatiyar <akatiyar@usc.edu>
2025-07-11 22:48:06 -0700

    Add About page with project and author details

---
+++ pages/3_About.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("📘 About Solace")
+st.markdown("""
+**Solace** is a cost and schedule prediction platform for NYC school construction projects.
+- Built using ML + Mistral
+- Visual breakdown by phase
+- Created by [Anushka Katiyar](https://www.linkedin.com/in/anushka-katiyar12/)
+- Source code: [GitHub](https://github.com/AnushkaKatiyar)
+""")
