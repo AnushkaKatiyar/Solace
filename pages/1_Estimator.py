830bd6db45c575bb1a42c2540d39dbf9066e9516
AnushkaKatiyar <akatiyar@usc.edu>
2025-07-11 22:45:57 -0700

    Add Lottie animation to Estimator page

--- pages/1_Estimator.py
+++ pages/1_Estimator.py
@@ -9,3 +9,16 @@ def to_excel(df):
 
 excel_data = to_excel(result_df)
 st.download_button("📥 Download Excel", excel_data, file_name="Solace_Estimates.xlsx")
+
+import streamlit_lottie as st_lottie
+import requests
+
+def load_lottie_url(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+lottie_url = "https://assets9.lottiefiles.com/private_files/lf30_editor_8zonlf.json"
+lottie_json = load_lottie_url(lottie_url)
+st_lottie(lottie_json, height=150)
