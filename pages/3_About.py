7d97f083fdb6f816c7c7e281198b363ce290de68
AnushkaKatiyar <akatiyar@usc.edu>
2025-07-20 23:53:56 -0700

    chatbot_110

--- pages/3_About.py
+++ pages/3_About.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.title("📘 About Solace")
+st.title("📘 About Solace NYC School Cost & Scheduling App")
 st.markdown("""
-**Solace** is a cost and schedule prediction platform for NYC school construction projects.
+**This App** is a cost and schedule prediction platform for NYC school construction projects.
 - Built using ML + Mistral
 - Visual breakdown by phase
 - Created for Solace Technologies as part of USC Viterbi Summer Smasher program
