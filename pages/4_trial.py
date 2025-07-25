83d3e58ff28340b0fffc899aac1ffa48748fc488
AnushkaKatiyar <akatiyar@usc.edu>
2025-07-24 20:11:20 -0700

    trial-60

--- pages/4_trial.py
+++ pages/4_trial.py
@@ -172,7 +172,7 @@ st.markdown("<div style='margin-top: 30px;'></div>", unsafe_allow_html=True)
 # Layout the three options side by side
 # Only show choices if nothing is selected yet
 if st.session_state.project_type is None:
-    spacer1, col1, col2, col3, spacer2 = st.columns([2, 2, 2, 2, 2])
+    spacer1, col1, col2, col3, spacer2 = st.columns([0.5, 2, 2, 2, 0.5])
     image_width = 100
     with col1:
         if st.button(" New Construction"):
